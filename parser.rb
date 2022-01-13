8a1686ee36fbffb2497451882a0e109bce0e6f55
Paulo Borba <pauloesb@gmail.com>
2022-01-13 14:49:31 -0300

    Add ARGV validation for execution

---
+++ parser.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "lib/parser/analyzer"
+
+if ARGV.empty?
+  warn "No file provided!"
+  warn "Usage: ./parser.rb webserver.log"
+  exit 1
+end
+
+begin
+  log_file_name = ARGV[0]
+  log_file = File.open(log_file_name, "r")
+rescue Errno::ENOENT
+  warn "Unable to open file: #{log_file_name}!"
+  exit 1
+end
