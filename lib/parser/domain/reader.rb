379a345f2d2c63fd1deaf11c48b7588affa01cae
Paulo Borba <pauloesb@gmail.com>
2022-01-13 22:52:39 -0300

    Add domain/reader with specs and fixture log files

---
+++ lib/parser/domain/reader.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative "../model/log_line"
+
+module Parser
+  class Reader
+
+    def self.run(log_file)
+      new(log_file).call
+    end
+
+    def call
+      read
+    end
+
+    def initialize(log_file)
+      check_log_file_instance(log_file)
+
+      @line_number = 0
+      @log_file = log_file
+    end
+
+    private
+
+    def read
+      @log_file.reduce([]) do |arr, parsed_log_line|
+        @line_number += 1
+
+        begin
+          arr << LogLine.new(@line_number, parsed_log_line)
+        rescue Parser::InvalidLogLine => e
+          warn "WARN - #{e.message}"
+          arr
+        end
+      end
+    end
+
+    def check_log_file_instance(log_file)
+      raise ArgumentError, "Parameter is not a File!" unless log_file.is_a?(File)
+    end
+  end
+end
+
---
+++ spec/lib/parser/domain/reader_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+RSpec.describe Parser::Reader do
+  describe "#run" do
+    subject { described_class.run(file) }
+
+    context "when parameter type is wrong" do
+      let(:file) { Struct.new("Test", :a) }
+
+      it "raises ArgumentError exception" do
+        expect { subject }.to raise_error(ArgumentError, "Parameter is not a File!")
+      end
+    end
+
+    context "when parameter type is correct" do
+      let(:filepath) { File.expand_path("./../../../fixtures/invalid.log", __FILE__) }
+      let(:file) { File.new(filepath) }
+
+      before do
+        allow_any_instance_of(described_class).to receive(:warn).and_return({})
+      end
+
+      it "return an array with valid LogLine instances" do
+        log_lines = subject
+
+        line_num = 0
+        file.seek(0)
+        file.reduce([]) do |arr, line|
+          begin
+            line_num += 1
+            arr << Parser::LogLine.new(line_num, line)
+          rescue Parser::InvalidLogLine
+            arr
+          end
+        end.each_with_index do |log_line, index|
+          expect(log_line.line).to eq(log_lines[index].line)
+        end
+      end
+    end
+  end
+end
+
